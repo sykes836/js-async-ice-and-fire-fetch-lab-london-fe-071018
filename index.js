75952e8c6cacb0edc34763050712f4b988245608
sykes836 <bernard.sykes836@gmail.com>
2018-09-02 16:41:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
-}
+
+
+fetch('https://anapioficeandfire.com/api/books')
+     .then(resp => resp.json())
+     .then(json => renderBooks(json));
+ }
+ 
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+   const main = document.querySelector('main')
+   json.forEach(book => {
+     const h2 = document.createElement('h2')
+     h2.innerHTML = `<h2>${book.name}</h2>`
+     main.appendChild(h2)
+   })
+ }
+ 
+ document.addEventListener('DOMContentLoaded', function() {
+   fetchBooks()
+   var books = fetchBooks();
+   console.log(books);
+ })
